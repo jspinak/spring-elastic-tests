0cb77410e043e13b6add2a95bd9fe06e420490c9
Joshua <jspinak@hotmail.com>
2023-12-14 15:06:03 +0100

    minor changes

--- src/main/java/brobot/elasticsearch/tests/LoadLib.java
+++ src/main/java/brobot/elasticsearch/tests/LoadLib.java
@@ -1,17 +1,23 @@
 package brobot.elasticsearch.tests;
 
-import org.springbootcourse.service.EmployeeServiceImpl;
+import io.github.jspinak.brobot.testingAUTs.ActionLog;
+import io.github.jspinak.brobot.testingAUTs.ActionLogSender;
+import io.github.jspinak.brobot.testingAUTs.StateTraversalService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoadLib {
-    private final EmployeeServiceImpl testClass;
+    private final StateTraversalService stateTraversalService;
+    private final ActionLogSender actionLogSender;
 
-    public LoadLib(EmployeeServiceImpl testClass) {
-        this.testClass = testClass;
+    public LoadLib(StateTraversalService stateTraversalService,
+                   ActionLogSender actionLogSender) {
+        this.stateTraversalService = stateTraversalService;
+        this.actionLogSender = actionLogSender;
     }
-
     public void doStuff() {
-        testClass.getAllEmployees();
+        stateTraversalService.traverseAllStates();
+        ActionLog actionLog = new ActionLog("DRAG2");
+        actionLogSender.sendActionLogToElastic(actionLog);
     }
 }
